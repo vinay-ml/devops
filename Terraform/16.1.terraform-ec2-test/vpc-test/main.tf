bfddd3ae0124e6b12dbb2299af58c517f84c8785
Vinay <vinay.mern@gmail.com>
2026-06-26 08:39:58 +0530

    changes are done

--- Terraform/16.1.terraform-ec2-test/vpc-test/main.tf
+++ Terraform/16.1.terraform-ec2-test/vpc-test/main.tf
@@ -1,6 +1,8 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    # source = "../terraform-aws-vpc"
+    source = "git::https://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@github.com/vinay-ml/devops.git//Terraform/16-terraform-aws-vpc?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = true
 }
+
