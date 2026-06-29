4b4f6bf764c2f03fb67cdcba250d199fdc182b76
Vinay <vinay.mern@gmail.com>
2026-06-29 09:37:37 +0530

    changes are done

--- Terraform/17.1.roboshop-dev-infra/40-databases/parameters.tf
+++ Terraform/17.1.roboshop-dev-infra/40-databases/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "sg_id" {
-  name  = "/${var.project}/${var.environment}/mysql_root_password"
+  name  = "/${var.project}/${var.environment}/mysql_root_password" 
   type  = "SecureString"
   value = var.mysql_root_password
   overwrite = true
